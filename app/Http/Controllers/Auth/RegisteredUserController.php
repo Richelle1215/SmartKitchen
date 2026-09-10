23712cda48642e1996f26ca88ead4cae5448d91b
Richelle Mendez <richellemendez35@gmail.com>
2026-09-10 11:37:22 +0800

    Fix: Set user role to 'registered' on registration so users can create recipes

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,6 +40,7 @@ class RegisteredUserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
+            'role' => 'registered', // Set role to registered for new users
         ]);
 
         event(new Registered($user));
