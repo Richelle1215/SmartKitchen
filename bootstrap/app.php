d22197e484ec9a64a5355b7661761f167e7fb253
Richelle Mendez <richellemendez35@gmail.com>
2026-09-26 23:23:51 +0800

    new

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,3 +23,4 @@ return Application::configure(basePath: dirname(__DIR__))
             fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
         );
     })->create();
+//26th day
